02e56f2a9b7cf7cb1f7fbbc0e8f7fbb171e8018c
Bryce Mecum <petridish@gmail.com>
2018-04-03 15:56:53 -0800

    More various whitespace and formatting fixups

--- R/extract_ea.R
+++ R/extract_ea.R
@@ -14,42 +14,39 @@ extract_ea <- function(paths, datetime = Sys.time()) {
 
     pkg_id <- xml2::xml_find_first(doc, "/eml:eml/@packageId") %>%
       xml2::xml_text()
+
     message(paste0("Extracting attributes from ", pkg_id))
 
-    entities <- lapply(
-      xml2::xml_find_all(doc, "//otherEntity | //dataTable"),
-      function(entity) {
-        entity_name <- xml2::xml_find_first(entity, ".//entityName") %>%
+    entities <- lapply(xml2::xml_find_all(doc, "//otherEntity | //dataTable"), function(entity) {
+      entity_name <- xml2::xml_find_first(entity, ".//entityName") %>%
+        xml2::xml_text()
+
+      attributes <- lapply(xml2::xml_find_all(entity, ".//attribute"), function(attribute) {
+        attribute_name <- xml2::xml_find_first(attribute, "./attributeName") %>%
+          xml2::xml_text()
+        attribute_labels <- xml2::xml_find_all(attribute, "./attributeLabel") %>%
+          xml2::xml_text() %>%
+          paste(collapse = " ")
+        attribute_def <- xml2::xml_find_first(attribute, "./attributeDefinition") %>%
+          xml2::xml_text()
+        attribute_unit <- xml2::xml_find_first(attribute, ".//unit/standardUnit | .//unit/customUnit") %>%
           xml2::xml_text()
 
-        attributes <- lapply(xml2::xml_find_all(entity, ".//attribute"),
-                             function(attribute) {
-                               attribute_name <- xml2::xml_find_first(attribute, "./attributeName") %>%
-                                 xml2::xml_text()
-                               attribute_labels <- xml2::xml_find_all(attribute, "./attributeLabel") %>%
-                                 xml2::xml_text() %>%
-                                 paste(collapse = " ")
-                               attribute_def <- xml2::xml_find_first(attribute, "./attributeDefinition") %>%
-                                 xml2::xml_text()
-                               attribute_unit <- xml2::xml_find_first(attribute, ".//unit/standardUnit | .//unit/customUnit") %>%
-                                 xml2::xml_text()
-                               #
-                               #                  # Clean up xml_find results
-                               attribute_labels <- ifelse(nchar(attribute_labels) == 0, NA_character_, attribute_labels)
+        # Clean up xml_find results
+        attribute_labels <- ifelse(nchar(attribute_labels) == 0, NA_character_, attribute_labels)
+        attribute_unit <- ifelse(nchar(attribute_unit) == 0, NA, attribute_unit)
 
-                               #                  attribute_unit <- ifelse(nchar(attribute_unit) == 0, NA, attribute_unit)
-
-                               # Return the result as a data.frame with all the info
-                               data.frame(packageId = pkg_id,
-                                          entityName = entity_name,
-                                          attributeName = attribute_name,
-                                          attributeLabel = attribute_labels,
-                                          attributeDefinition = attribute_def,
-                                          attributeUnit = attribute_unit)
-                             })
-
-        do.call(rbind, attributes)
+        # Return the result as a data.frame with all the info
+        data.frame(packageId = pkg_id,
+                   entityName = entity_name,
+                   attributeName = attribute_name,
+                   attributeLabel = attribute_labels,
+                   attributeDefinition = attribute_def,
+                   attributeUnit = attribute_unit)
       })
+
+      do.call(rbind, attributes)
+    })
 
     entities <- entities[which(!is.na(entities))]
 
@@ -84,6 +81,6 @@ extract_ea <- function(paths, datetime = Sys.time()) {
 #' @importFrom magrittr "%>%"
 #' @export
 ea_to_csv <- function(paths,
-                       datetime = Sys.time()) {
+                      datetime = Sys.time()) {
   .Deprecated("extract_ea")
 }
